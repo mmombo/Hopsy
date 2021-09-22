cd9d4c2632edaa37119802a24483693bd78bd1bb
Matt Mombourquette <mmombou@uwo.ca>
2021-09-21 21:31:13 -0400

    updated mobile navbar menu styles,changed show page to only pass geometry object from brewery to the showMap.js script.

--- static/js/showMap.js
+++ static/js/showMap.js
@@ -1,15 +1,15 @@
-brewery = JSON.parse(brewery);
+breweryGeo = JSON.parse(breweryGeo);
 
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/mapbox/dark-v10", // stylesheet location
-  center: brewery.geometry.coordinates, // starting position [lng, lat]
+  center: breweryGeo.coordinates, // starting position [lng, lat]
   zoom: 13, // starting zoom
 });
 //prettier-ignore
 const marker = new mapboxgl.Marker()
-    .setLngLat(brewery.geometry.coordinates)
+    .setLngLat(breweryGeo.coordinates)
     .addTo(map);
 
 map.addControl(new mapboxgl.NavigationControl());
